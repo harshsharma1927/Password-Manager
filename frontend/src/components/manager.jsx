3f4d1b60db0e2f12385bdf3bc19dda1c4ea2bb61
Harsh Sharma <hars27sharma@gmail.com>
2026-01-16 18:33:03 +0530

    bug fixes

--- frontend/src/components/manager.jsx
+++ frontend/src/components/manager.jsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import axios from "axios";
 import 'react-toastify/dist/ReactToastify.css';
 
-const API_URL = process.env.API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 const Manager = () => {
